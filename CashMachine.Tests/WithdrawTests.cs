33df2a4257a96648f0093ef3ff2d7721c53d915d
Emerson Soares <on.emersonsoares@gmail.com>
2013-10-03 01:11:54 -0300

    Withdraw tests

---
+++ CashMachine.Tests/WithdrawTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CashMachine.Tests
+{
+    [TestClass]
+    public class WithdrawTests
+    {
+        [TestMethod]
+        public void TestReturnOfNotes()
+        {
+            var withdraw = new Withdraw(1, 1, 1, 1);
+
+            Assert.AreEqual(1, withdraw.TenNotes);
+            Assert.AreEqual(1, withdraw.TwentyNotes);
+            Assert.AreEqual(1, withdraw.FiftyNotes);
+            Assert.AreEqual(1, withdraw.HundredNotes);
+        }
+    }
+}
